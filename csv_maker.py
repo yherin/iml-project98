8031a419567599f88d47bc92f08d6ee5909239bf
Jack Sheridan <jack.sheridan@helsinki.fi>
2021-12-10 13:05:06 +0200

    update csv functionality

--- csv_maker.py
+++ csv_maker.py
@@ -3,28 +3,20 @@ from typing import Type
 import pandas as pd
 from pandas import DataFrame
 
-class csv_writer:
-
-    def writeFile(self, filePath: str, data: DataFrame, modelScore: float):
-        self._run_tests(data, modelScore)
-
-        fo = open(filePath, 'w', encoding='UTF-8')
-        fo.write(f'{modelScore}\n')
-        fo.close()
-        sleep(1)
-        fo = open(filePath, 'a', encoding='UTF-8')
-        data.to_csv(fo, sep=',', index=False, index_label=False)
-        fo.close()
-        print(f'File written to {filePath}')
-
-    def _run_tests(self, data, modelScore):
+def writeFile( filePath: str, data: DataFrame, modelScore: float):
+    """
+    filePath: path to write to
+    data: Dataframe containing 'class4' and 'p' columns
+    modelScore: estimate binary classification accuracy
+    """
+    def _run_tests( data, modelScore):
         if not type(modelScore) is float:
             raise TypeError('pass float to modelScore')
         if not isinstance(data, DataFrame):
             raise TypeError('pass data as a pandas dataframe')
-        if 'class4' not in data.columns.names:
+        if 'class4' not in data.columns:
             raise AttributeError('class4 column is missing')
-        if 'p' not in data.columns.names:
+        if 'p' not in data.columns:
             raise AttributeError('p column is missing')
         if len(data.columns) > 2:
             raise AttributeError('pass only two columns: class4 and p')
@@ -32,5 +24,21 @@ class csv_writer:
             raise AttributeError('some probabilities in the dataframe are > 0.99')
         if data['p'].lt(0).any():
             raise AttributeError('some probabilities in the dataframe are negative')
-        if (data['class4'] not in ['Ia', 'Ib', 'II', 'nonevent']).any():
+        if not (data['class4'].isin(['Ia', 'Ib', 'II', 'nonevent']).all()):
             raise AttributeError('incorrect class labels found')
+            #only labels 'Ia', 'Ib', 'II', 'nonevent' are valid
+    _run_tests(data, modelScore)
+
+    fo = open(filePath, 'w', encoding='UTF-8')
+    fo.write(f'{modelScore}\n')
+    fo.close()
+    sleep(1)
+    fo = open(filePath, 'a', encoding='UTF-8')
+    data.to_csv(fo, sep=',', index=False, index_label=False)
+    fo.close()
+    print(f'File written to {filePath}')
+
+#testing
+filePath='test.csv'
+dataz = pd.DataFrame({'class4': ['Ia', 'Ib', 'II', 'Ia', 'nonevent' ], 'p': [0.3, 0.4, 0.99, 0.01, 0.512125]})
+writeFile(filePath, dataz, 0.3)
